a38aa4d0a8c3ff671a0d67e6603d022a3c216c7a
Alexey Peskov <aspeskov@gmail.com>
2020-10-11 17:07:45 +0200

    Database ready app

---
+++ src/main/java/com/sandlex/progressbot/ProgressbotApplication.java
@@ -0,0 +1,13 @@
+package com.sandlex.progressbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgressbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgressbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sandlex/progressbot/ProgressbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandlex.progressbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgressbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
